654b4f6a4aed5fc78d1d62e164811e887375f6fe
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-15 20:33:59 +0330

    working on listCart

--- src/components/ListCarts.js
+++ src/components/ListCarts.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const ListCarts = () => {
+import { FaCheckCircle } from "react-icons/fa";
+import { FaRedo } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+
+
+
+const ListCarts = ({data}) => {
     return (
         <div className='cart'>
-            
+            {data.desc}
+
+            <div>
+                <button>
+                    <FaCheckCircle />
+                </button>
+
+                <button>
+                    <FaTrashAlt />
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 import ListCarts from './ListCarts';
 
-const Lists = () => {
+const Lists = ({todos}) => {
     return (
         <div>
-            
+            {
+                todos.length ?
+                todos.map(item=> <ListCarts key={item.id} data={item} />)
+                :
+                <div style={{background:'crimson', 
+                padding:'3px',
+                borderRadius:'3px',
+                color:"#fff", 
+                textAlign:'center'}}>
+                    <p>there is no idea</p>
+                </div>
+            }
         </div>
     );
 };
